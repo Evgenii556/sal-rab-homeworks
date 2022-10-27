7950c57a5f6aa4417926c6e3e804e536f1d7c783
Evgenii <Ev@gmail.com>
2022-10-27 16:00:19 +0500

    Task_Final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,12 +34,16 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    let data = {client: {name, phone}, order: {address, sum}, goods: [{title, count}]
+};
+
+
 
     let countOfGoods = goods.length;
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
+    for (let i = 0; i < countOfGoods; i++) {
         data.goods.push(goods[i].title);
+        data.goods.push(goods[i].count);
     }
 
     data.order.address = address;
@@ -47,7 +51,7 @@ function sendRequest(name, phone, address, goods, sum) {
 
     data.client = 'Иван';
 
-    let jsonData = JSON.stringify(data);
+    let jsonData = JSON.stringify({data: data});
 
     return jsonData;
 }
